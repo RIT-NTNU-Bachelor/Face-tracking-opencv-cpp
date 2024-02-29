25879b7fc7d7ea60d6bd0a052ece13d02e892ef0
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-02-29 18:43:43 -0500

    add: distance drawer between boundary box and the left most border - rendering x axis

--- src/main.cpp
+++ src/main.cpp
@@ -37,6 +37,67 @@ void detect_faces(Mat& img)
      rectangle(img, face.tl(), face.br(), Scalar(255, 0, 255), 3);
 }
 
+void draw_x_distance(Mat& img)
+{
+
+  CascadeClassifier face_cascade;
+
+  face_cascade.load("models/haarcascade_frontalface_default.xml");
+  
+
+  if(face_cascade.empty())
+    cout << "Classifier has not been loaded!\n";
+
+  // creating rect for printing bounding box around detected faces
+  vector<Rect> faces;
+  
+  // Detects objects of different sizes in the input image.   
+  // The detected objects are returned as a list of rectangles.
+  face_cascade.detectMultiScale(img, faces, 1.1,10); 
+ 
+  // drawing bounding box around detected faces
+  for(auto& face : faces){
+    // Drawing a line from the left border to the right border for each detected face
+
+    // Calculate the vertical center of the face
+    int y = face.y + face.height / 2;
+
+    // Calculate the center of the face 
+    int x = face.x + face.width/2;
+
+    // Draw a line from the left border (x=0) to the right border (x=img.cols)
+    line(img, Point(0, y), Point(x, y), Scalar(0, 0, 255), 3);
+
+    // Add the infomation of the length of x
+    string image_text = "X-distance: " + to_string(x) + " unit";
+
+    cv::putText(img, //target image
+            image_text, //text
+            cv::Point(0, y-20), //top-left position
+            cv::FONT_HERSHEY_DUPLEX,
+            1,
+            CV_RGB(0, 0, 0), //font color
+            2);
+
+
+    // Add percentage text
+    int width = img.cols; 
+    float percentage = (static_cast<float>(x) / width) * 100;
+
+    // Round to 2 decimal places
+    percentage = roundf(percentage*100)/100;
+    image_text = to_string(percentage) + "%";
+    cv::putText(img, //target image
+            image_text, //text
+            cv::Point(0, img.rows - 20), //top-left position
+            cv::FONT_HERSHEY_DUPLEX,
+            1,
+            CV_RGB(0, 0, 0), //font color
+            2);
+  }
+
+}
+
 void detect_eyes(Mat& img){
     CascadeClassifier eye_cascade;
     eye_cascade.load("models/haarcascade_eye.xml");
@@ -83,7 +144,7 @@ int main(){
         v_cap.read(img);            
 
         // Detect faces and eyes
-        detect_faces(img);
+        draw_x_distance(img);
         add_text_data(img);
 
 
