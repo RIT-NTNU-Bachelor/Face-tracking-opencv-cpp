9fb3760c475f1fddaad01b7ce71b7859b2ecf49a
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-02-29 19:23:44 -0500

    add: distance estimation

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,16 @@
 using namespace cv;
 using namespace std;
 
+
+// Constants that may change over time as the app
+
+int x_size = 0;
+int y_size = 0;
+
 // Function to draw bounding box abound detected frontal face
 // Input: image
 // Output: void
+
 void detect_faces(Mat& img)
 {
 
@@ -98,6 +105,67 @@ void draw_x_distance(Mat& img)
 
 }
 
+// Function to draw bounding box abound detected frontal face
+// Input: image
+// Output: void
+void detect_depth(Mat& img, bool calibrate)
+{
+
+  CascadeClassifier face_cascade;
+
+  face_cascade.load("models/haarcascade_frontalface_default.xml");
+
+
+  if(face_cascade.empty())
+    cout << "Classifier has not been loaded!\n";
+
+  // creating rect for printing bounding box around detected faces
+  vector<Rect> faces;
+  
+  // Detects objects of different sizes in the input image.   
+  // The detected objects are returned as a list of rectangles.
+  face_cascade.detectMultiScale(img, faces, 1.1,10); 
+ 
+  // drawing bounding box around detected faces
+  for(auto& face : faces){
+    rectangle(img, face.tl(), face.br(), Scalar(255, 0, 255), 3);
+
+    if (calibrate){
+        x_size = face.width;
+        y_size = face.height;
+        calibrate = false;
+    }
+
+    string image_text;
+
+    // If the depth has been calibrated 
+    if (x_size != 0 && y_size != 0){
+        int box_size = face.width * face.height;
+        int org_box_size = x_size * y_size;
+
+        float percentage = (static_cast<float>(org_box_size) / box_size) * 100;
+        // Round to 2 decimal places
+        percentage = roundf(percentage*100)/100;
+
+        image_text = "Z = " + to_string(percentage) + " m";
+    }else {
+        // Else set unknown z distance 
+        image_text = "Z = ?";
+    }
+
+    // Put distance text at the bottom of the box that is around the face
+    cv::putText(img, 
+        image_text, 
+        cv::Point(face.x, face.y + face.width + 25),
+        cv::FONT_HERSHEY_DUPLEX,
+        0.7,
+        CV_RGB(0, 150, 150),
+        2);
+
+  }
+     
+}
+
 void detect_eyes(Mat& img){
     CascadeClassifier eye_cascade;
     eye_cascade.load("models/haarcascade_eye.xml");
@@ -138,17 +206,27 @@ void add_text_data(Mat& img){
 int main(){
     VideoCapture v_cap(0);    // 0: default webcam
     Mat img;
-        
+    
+    bool calibrate = false;
     while (true){                 //once v_cap frame read is over
     
         v_cap.read(img);            
 
         // Detect faces and eyes
-        draw_x_distance(img);
+        detect_depth(img, calibrate);
         add_text_data(img);
 
 
         imshow("Detected Faces", img);  // display img
+
+        int key = cv::waitKey(30);
+        if (key == 32) { // ASCII value for spacebar is 32
+            calibrate = true;
+        } else {
+            calibrate = false;
+        }
+
+
         waitKey(10);                    // delay of 10 ms 
     }
     return 0;
